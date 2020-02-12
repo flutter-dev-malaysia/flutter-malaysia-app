a7d5cf374ff8ee01854d818a2254c9e993090e68
Haitham <lion4h@hotmail.com>
2020-02-12 11:19:59 +0800

    basic home screen elements, no style yet

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:flutter_my_meeting/widgets/meetupCard.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,9 +12,72 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Hello World!'),
+        body: SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width * .05),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * .9,
+              child: const Image(
+                image: AssetImage('lib/assets/images/logo.png'),
+              ),
+            ),
+            Container(
+              child: Text(
+                'Next Meetup',
+                style: TextStyle(fontSize: 24, color: Colors.grey[800]),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 5),
+              width: MediaQuery.of(context).size.width * .9,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+                color: Colors.lightBlue[200],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    'Flutter Cyberjaya MeetUp #2',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(right: 8, bottom: 6),
+                        child: Icon(FontAwesomeIcons.calendarAlt, size: 20),
+                      ),
+                      const Text(
+                        'To be announced',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(
+                "Previous Meetup's",
+                style: TextStyle(fontSize: 24, color: Colors.grey[800]),
+              ),
+            ),
+            const MeetupCard(
+              photoUrl:
+                  'https://lh3.googleusercontent.com/3dtkK3kpns2jY6m73CKgqppE_WS6SkoBxEYQQE5GmjWDT2XIc-KoR8NpwZ4kRW4FSIvX8rSNUMDmIYe5Rd59jiAOVQc8BSCf8Ef8Z6U15WzIb-BXfg3nmYnQ0ThPcGeZQ-a3FxHHSv5LSP4jkqQN-9yTavfcZtLuGAI_XWpCJaRH6SAt1xdNSEkzkvpEbFp7NkMPRqnGMR2MSIP-5T7Wd_IKGp3-M019zBDV7JtSjZ6ToNBPrZasUQ4Hlz1UkAtEPGSgrcfCwmrB1DqIUxVEvPiRtqZ9GAF5OEFAIARwe0mdKSLOdyornupBojdpIUSzSpOmA-MHISCyfQidvllcmSu-6aWhhebndNQmuOtpfol9tvwKyB591Lp2vNfKX7YgHDLLj6rwisX7bb9QzjsMxoed1zKy6qNZ0EFEik4O0T09aGst06x0QaFqsW-yc9BYgggPQHzs2ZpUgQsL_nJ3dih4XnwweUKcbK88jZdm0UKAA5Dv3q2CtANaja-b2iT8oP7i3hjoAU_t1dtJKU6xgLYSiam42L6a7noFvpYyB8juE00P_feoRtYOrwB_tid-fAMey0Wnl8Tfrv_jvfNZRefmHFx4egg_3IwPZoXqBm1K1H-vUHCtYDkWvg4a4NvXkyq-4FRejViOpNYFF6Zq3YsCJbrIR1Zy7nXB3865Kqhenr8NdtD2v2E4-u-WdqxnC7m83g-SkHkvj9V5T8iM4_TL9L2JK9cRO7RtLx1joLkvM_aG=w1214-h910-no',
+              title: 'Flutter Cyberjaya MeetUp #1',
+              date: 'WED, FEB 5, 6:30 PM',
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
